d0a93b5324a151064cc7ba2d3426a12dcc73baf0
Tristean <861907286@qq.com>
2017-12-02 23:07:21 +0800

    add files

--- Second_Car/src/main/java/com/tg/daoImp/TG_CarDaoImp.java
+++ Second_Car/src/main/java/com/tg/daoImp/TG_CarDaoImp.java
@@ -18,20 +18,45 @@ public class TG_CarDaoImp implements TG_CarDao {
 		return sessionFactory.getCurrentSession();
 	}
 	
-
+	//按id删除
 	@Override
 	public boolean deleteById(int id) {
 		// TODO Auto-generated method stub
-		return false;
+		try{
+			String hql;
+			Query query;
+			
+			hql="delete from TG_Car where cid=?";
+			query=getSession().createQuery(hql);
+			query.setInteger(0, id);
+			query.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		System.out.println("delete");
+		return true;
 	}
-	
+	//删除所有
 	@Override
 	public boolean deleteAll() {
 		// TODO Auto-generated method stub
-		return false;
+		try{
+			String hql;
+			Query query;
+			
+			hql="delete from TG_Car";
+			query=getSession().createQuery(hql);
+			query.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		System.out.println("delete all");
+		return true;
 	}
 
-	
+	//添加新汽车
 	@Override
 	public boolean add(TG_Car car) {
 		// TODO Auto-generated method stub
@@ -39,7 +64,7 @@ public class TG_CarDaoImp implements TG_CarDao {
 		return true;
 	}
 
-
+	//按序号查找
 	@Override
 	public List selectById(int id) {
 		// TODO Auto-generated method stub
@@ -58,7 +83,7 @@ public class TG_CarDaoImp implements TG_CarDao {
 		
 		return tgc_list;
 	}
-
+	//查询所有
 	@Override
 	public List selectAll() {
 		// TODO Auto-generated method stub
